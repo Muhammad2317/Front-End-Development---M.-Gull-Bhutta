4ad436ef14e2895efe043f198794aec408b9d548
Muhammad <120671402+Muhammad2317@users.noreply.github.com>
2023-02-20 13:33:37 +0000

    Delete Whish List App directory

--- Whish List App/src/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './style.css';
-
-const Footer = (props) => {
-  return (
-    <footer><h1>{props.length} List {props.length === 1 ? 'Item' : 'items'}</h1></footer>
-  )
-};
-export default Footer;
